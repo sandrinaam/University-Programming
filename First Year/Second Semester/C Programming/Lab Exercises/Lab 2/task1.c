7f1c7930cd7fc4f4a47366e6c1c88e86b6e26026
Sandrina Milanova <sandrina06@abv.bg>
2026-05-13 17:55:36 +0300

    Update task1.c

--- First Year/Second Semester/C Programming/Lab Exercises/Lab 2/task1.c
+++ First Year/Second Semester/C Programming/Lab Exercises/Lab 2/task1.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
  
 int main() {
-   int n;
-   printf("n=");
-   scanf("%d", &n);
-   if (n >6)
-   printf("n e >6");
+    int n;
+    printf("n=");
+    scanf("%d", &n);
+ 
+    for(int counter=1;counter <=n;counter ++){
+      for(int num=1; num<= counter; num++){
+          printf("%d ", counter);
+      }
+      printf("\n");
+    }
  
     return 0;
 }
