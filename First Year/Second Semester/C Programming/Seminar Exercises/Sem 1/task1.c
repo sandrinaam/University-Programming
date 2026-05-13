5d98eff1cff9708e52a27c859752a769c4ca40df
Sandrina Milanova <sandrina06@abv.bg>
2026-05-13 17:47:56 +0300

    Create task1.c

---
+++ First Year/Second Semester/C Programming/Seminar Exercises/Sem 1/task1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+    char s;
+ 
+    printf("Enter a symbol:");
+    scanf("%c", &s);
+    printf("%c%c%c%c%c%c%c%c%c%c\n",s,s,s,s,s,s,s,s,s,s);
+    printf("%c        %c\n",s,s);
+    printf("%c        %c\n",s,s);
+    printf("%c        %c\n",s,s);
+    printf("%c%c%c%c%c%c%c%c%c%c\n",s,s,s,s,s,s,s,s,s,s);
+ 
+ 
+ 
+    return 0;
+}
