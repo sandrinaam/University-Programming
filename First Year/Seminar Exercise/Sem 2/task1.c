b6d54b3d53b3761e49323520f05d46ad4f76b230
Sandrina Milanova <sandrina06@abv.bg>
2026-05-13 17:05:11 +0300

    Create task1.c

---
+++ First Year/Seminar Exercise/Sem 2/task1.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int greatest_number(int a, int b) {
+    int c;
+    if (a > b) c = a;
+    else c = b;
+    return c;
+}
+
+int smallest_number(int a, int b) {
+    int d;
+    if (a < b) d = a;
+    else d = b;
+    return d;
+}
+
+
+int main() {
+
+    int a;
+    int b;
+
+    printf("Enter 2 numbers:");
+    scanf("%d%d",&a, &b);
+
+    int c = greatest_number(a,b);
+    int d = smallest_number(a,b);
+
+
+    printf("The smallest number is: %d\n",c);
+    printf("The largest number is: %d\n",d);
+
+
+    while (1) {
+        int new;
+        printf("Enter number:");
+        scanf("%d",&new);
+
+        if (new == 0) {
+            break;
+        }
+
+        else if (new > c) {
+            printf("The largest number is: %d\n",new);
+
+        }
+        else if (new < d) {
+            printf("The smallest number is: %d\n",new);
+        }
+
+
+    }
+
+
+
+
+    return 0;
