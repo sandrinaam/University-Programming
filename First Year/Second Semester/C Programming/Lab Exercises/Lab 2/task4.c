6bf9b313b00945c654c99b399ba559f546d09100
Sandrina Milanova <sandrina06@abv.bg>
2026-05-13 17:56:40 +0300

    Create task4.c

---
+++ First Year/Second Semester/C Programming/Lab Exercises/Lab 2/task4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ 
+int main() {
+    int n;
+    int k;
+ 
+    printf("n=");
+    scanf("%d", &n);
+    printf("k=");
+    scanf("%d", &k);
+ 
+    for(n){
+      printf("num=")
+      scanf("%d", num)
+      }
+    }
+ 
+    int count = 0
+ 
+ 
+    return 0;
+}
